3ddbb6bac0c965251fc17042d77dc01e916f2a32
Max Soe <maxsoe@gmail.com>
2016-08-26 18:20:27 +0100

    Error - Routing.
    Can’t find the error. Going to do a diff with Plunker

--- app/hero.service.js
+++ app/hero.service.js
@@ -8,8 +8,8 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var core_1 = require('@angular/core');
 var mock_heroes_1 = require('./mock-heroes');
+var core_1 = require('@angular/core');
 var HeroService = (function () {
     function HeroService() {
     }
@@ -18,10 +18,14 @@ var HeroService = (function () {
     };
     HeroService.prototype.getHeroesSlowly = function () {
         return new Promise(function (resolve) {
-            return setTimeout(function () { return resolve(mock_heroes_1.HEROES); }, 5000);
-        } // 5 seconds
-         // 5 seconds
+            return setTimeout(function () { return resolve(mock_heroes_1.HEROES); }, 2000);
+        } // 2 seconds
+         // 2 seconds
         );
+    };
+    HeroService.prototype.getHero = function (id) {
+        return this.getHeroes()
+            .then(function (heroes) { return heroes.find(function (hero) { return hero.id === id; }); });
     };
     HeroService = __decorate([
         core_1.Injectable(), 
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,15 +1,21 @@
-import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { Injectable } from '@angular/core';
+
 @Injectable()
 export class HeroService {
   getHeroes(): Promise<Hero[]> {
     return Promise.resolve(HEROES);
   }
+
   getHeroesSlowly(): Promise<Hero[]> {
     return new Promise<Hero[]>(resolve =>
-      setTimeout(() => resolve(HEROES), 5000) // 5 seconds
+      setTimeout(() => resolve(HEROES), 2000) // 2 seconds
     );
   }
 
+  getHero(id: number): Promise<Hero> {
+    return this.getHeroes()
+               .then(heroes => heroes.find(hero => hero.id === id));
+  }
 }
