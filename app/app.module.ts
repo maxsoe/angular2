450445753e4ed7ed2e4a6191e3afb176888eca70
Max Soe <maxsoe@gmail.com>
2016-09-10 13:38:52 +0100

    Step 7 - HTTP
    Completed

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,19 +1,18 @@
+import './rxjs-extensions';
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { HttpModule }    from '@angular/http';
-
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
-
 import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard.component';
 import { HeroesComponent }      from './heroes.component';
 import { HeroDetailComponent }  from './hero-detail.component';
 import { HeroService }          from './hero.service';
+import { HeroSearchComponent }  from './hero-search.component';
 import { routing }              from './app.routing';
-
 @NgModule({
   imports: [
     BrowserModule,
@@ -27,6 +26,7 @@ import { routing }              from './app.routing';
     DashboardComponent,
     HeroDetailComponent,
     HeroesComponent,
+    HeroSearchComponent
   ],
   providers: [
     HeroService,
